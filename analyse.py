a8f1d646f0d50f480d314c9374eaa06ad49a5ad9
Lawrence Phillips <lawphi56@gmail.com>
2017-01-13 14:03:42 +0000

    lookahead working, building analyser/sorter

--- analyse.py
+++ analyse.py
@@ -11,9 +11,9 @@ times = pkl.load(open('times.pkl', 'rb'))
 
 
 def get_means_devs(X, window_len):
-    means = [st.gmean(X[:i+1]) for i in range(window_len)]
+    means = [np.mean(X[:i+1]) for i in range(window_len)]
     devs = [np.sqrt(st.moment(X[:i+1], moment=2)) for i in range(window_len)]
-    means += [st.gmean(X[i+1: i+window_len+1])
+    means += [np.mean(X[i+1: i+window_len+1])
               for i in range(len(X)-window_len)]
     devs += [np.sqrt(st.moment(X[i+1: i+window_len+1], moment=2))
              for i in range(len(X)-window_len)]
@@ -49,3 +49,24 @@ def lookahead(X, means, devs, fac, stride, skip):
                    and not X[i-1+skip] > (means[i-1+skip]
                                           + fac * devs[i-1+skip])]
     return lower_diffs, upper_diffs
+
+
+def make_lookahead_array(prices, md_dict):
+    windows = (np.arange(9)+np.ones([9]))*10
+    windows = np.concatenate((windows, (np.arange(9)+np.ones([9]))*100))
+    windows = np.concatenate((windows, (np.arange(9)+np.ones([9]))*1000))
+    windows = windows.astype(np.int_)
+
+    strides = (np.arage(9) + np.ones([9]))
+    strides = np.concatenate((strides, (np.arange(9)+np.ones([9]))*10))
+    strides = np.concatenate((strides, (np.arange(9)+np.ones([9]))*100))
+    strides = np.concatenate((strides, (np.arange(9)+np.ones([9]))*1000))
+    strides = strides.astype(np.int_)
+
+    facs = [(i+1)*4/30 for i in range(len(30))]
+    return [((window, stride, fac),
+             lookahead(prices, md_dict[window][0], md_dict[window][1],
+                       fac, stride, stride))
+            for window in windows
+            for fac in facs
+            for stride in strides]

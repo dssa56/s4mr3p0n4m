a1320f85c7c06de111192dcd70264e331d063d57
dssa56 <lawphi56@gmail.com>
2017-03-03 19:46:57 +0100

    testing bot on fake data

--- practice_bot/bot.pyx
+++ practice_bot/bot.pyx
@@ -1,25 +1,46 @@
 import numpy as np
 
 
-def train_bot(double[:] pr, int[:] mm, int[:] ln, int[:] prev,
-  double alpha, double gamma, double ep):
+def train_bot(double[:] pr, int[:] mm, int[:] ln,
+  double alpha, double gamma, double ep, int n_epochs):
   cdef:
     double[:,::1] book = np.zeros([4, 2], order='C')
-    double[:,:,:,:,::1] avf = np.ones([2, 10, 11, 3, 9], order='C') * 10
+    double[:,:,:,:,::1] avf = np.ones([2, 10, 11, 3, 13], order='C') * 10
     int[:] state = np.array([mm[0], ln[0], b_to_ind(book), 0], dtype=np.int32)
     int[:] newstate = np.empty_like(state)
     int i = 0
     int action
     double reward
     double spread = 0.00015
+  for _ in range(n_epochs):
+    for i in range(len(pr) - 1):
+      sort_book(book, state, pr[i])
+      newstate, action, reward = (
+        transition(state, pr, mm, ln, book, avf, i, ep, spread)
+      )
+      update(avf, state, newstate, book, action, reward, alpha, gamma)
+      state = newstate[:]
+  return avf
+
+
+def implament_policy(double[:] pr, int[:] mm, int[:] ln,
+  double[:,:,:,:,::1] avf):
+  cdef:
+    double[:,::1] book = np.zeros([4, 2], order='C')
+    int[:] state = np.array([mm[0], ln[0], b_to_ind(book), 0], dtype=np.int32)
+    int[:] newstate = np.empty_like(state)
+    double reward
+    double[:] rewards = np.empty([len(pr) - 1])
+    int[:] actions = np.empty([len(pr) - 1], dtype=np.int32)
   for i in range(len(pr) - 1):
     sort_book(book, state, pr[i])
     newstate, action, reward = (
-      transition(state, pr, mm, ln, book, avf, i, ep, spread)
+     transition(state, pr, mm, ln, book, avf, i, 0, 0.00015)
     )
-    update(avf, state, newstate, book, action, reward, alpha, gamma)
+    actions[i] = action
+    rewards[i] = reward
     state = newstate[:]
-  return avf
+    return np.asarray(actions), np.asarray(rewards)
 
 
 def test_sort_book():
@@ -51,7 +72,7 @@ cdef sort_book(double[:,::1] book, int[:] state, double pr):
 
 def test_update():
   cdef:
-    double[:,:,:,:,::1] avf = np.zeros([2, 10, 11, 3, 9], order='C')
+    double[:,:,:,:,::1] avf = np.zeros([2, 10, 11, 3, 13], order='C')
     int[:] state = np.array([1, 2, 0, 0], dtype=np.int32)
     int[:] newstate = np.array([1, 3, 0, 0], dtype=np.int32)
     double[:,::1] book = np.zeros([4, 2], order='C')
@@ -64,16 +85,12 @@ def test_update():
   return avf[1][2][0][0][0]
 
 
-
 cdef update(double[:,:,:,:,::1] avf, int[:] state,
   int[:] newstate, double[:,::1] book, int action, double reward, double alpha,
   double gamma):
   ud = alpha * (reward + gamma * maxq(avf, newstate, book))
-  print(ud)
   ud -= alpha * avf[state[0]][state[1]][state[2]][state[3]][action]
-  print(ud)
   avf[state[0]][state[1]][state[2]][state[3]][action] += ud
-  print(avf[state[0]][state[1]][state[2]][state[3]][action])
 
 
 cdef maxq(double[:,:,:,:,::1] avf, int[:] state, double[:,::1] book):
@@ -85,14 +102,30 @@ cdef maxq(double[:,:,:,:,::1] avf, int[:] state, double[:,::1] book):
   return ava[np.argmax(avflist)]
 
 
+def test_transition():
+  cdef:
+    double[:,::1] book = np.array([[1, 1.01], [1, 1.05], [0, 0], [0, 0]], order='C')
+    int[:] state = np.array([0, 7, b_to_ind(book), 2], dtype=np.int32)
+    double[:] pr = np.array([1.02, 1.03])
+    int[:] mm = np.array([1, 1], dtype=np.int32)
+    int[:] ln = np.array([0, 1], dtype=np.int32)
+    double[:,:,:,:,::1] avf = np.zeros([2, 10, 11, 3, 13], order='C')
+    int i = 0
+    double ep = 0.1
+    double spread = 0.001
+  avf[0][7][b_to_ind(book)][1][9] = 1
+  newstate, action, reward = transition(state, pr, mm, ln, book, avf, i, ep, spread)
+  return newstate, action, reward, np.asarray(book)
+
+
 cdef transition(int[:] state, double[:] pr, int[:] mm, int[:] ln,
   double[:,::1] book, double[:,:,:,:,::1] avf, int i, double ep,
   double spread):
   cdef:
     double reward = 0
-    int[4] newstate
-    int[:] ava = get_ava(book, state[3])
-    double[:] avflist = np.empty_like(ava)
+    int[:] newstate = np.empty([4], dtype=np.int32)
+    int[:] ava = np.array(get_ava(book, state[3]), dtype=np.int32)
+    double[:] avflist = np.empty_like(ava, dtype=np.float64)
     int j = 0
     int bestaction
     int action
@@ -183,13 +216,15 @@ cdef get_ava(double[:,::1] book, int mode):
 cdef inline long_rew(double[:,::1] book, double pr, int i, int mode, int upd,
   double spread):
   if mode == 1:
-    r =  1 - book[i][0]*book[i][1]/pr - spread
+    r =  book[i][0] - book[i][0]*book[i][1]/(pr - spread)
     if upd == 1:
-      book[i] = np.array([0, 0])
+      book[i][0] = 0.
+      book[i][1] = 0.
     return r
-  r = book[i][0]*book[i][1]/pr - 1 - spread
+  r = book[i][0]*book[i][1]/(pr + spread) - book[i][0]
   if upd == 1:
-    book[i] = np.array([0, 0])
+    book[i][0] = 0.
+    book[i][1] = 0.
   return r
 
 
@@ -197,21 +232,27 @@ cdef update_book(double[:,::1] book, int[:] state, int[:] newstate,
   int action, double pr, double spread):
   cdef int i = 0, n_pos = 0
   if action == 0:
+    newstate[3] = state[3]
     return 0
   elif action < 5:
-    book[3] = np.array([action, pr])
+    book[3][0] = action
+    book[3][1] = pr
     newstate[3] = 1
     return 0
   elif action < 9:
-    book[3] = np.array([action - 4, pr])
+    book[3][0] = action - 4
+    book[3][1] = pr
     newstate[3] = 2
+    return 0
   else:
-    r = long_rew(book, pr, action - 5, state[3], 1, spread)
+    r = long_rew(book, pr, action - 9, state[3], 1, spread)
     for i in range(4):
       if book[i][0] != 0:
         n_pos += 1
     if n_pos == 0:
       newstate[3] = 0
+    else:
+      newstate[3] = state[3]
     return r
 
 
